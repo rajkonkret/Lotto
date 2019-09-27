b912b666ca6eebadce469cced11fe5e8a9a8bce6
rajkonkret <rajkonkret@hotmail.com>
2019-09-27 10:19:35 +0200

    testowa

--- src/Lotto.java
+++ src/Lotto.java
@@ -8,11 +8,12 @@ public class Lotto {
         int j;
         int result;
         Random rnd = new Random();
-        System.out.println("Generator Lotto");
-        for (j=0; j<100; j++) {
+        System.out.println("Generator Lotto by RJ");
+        for (j=1; j<=100; j++) {
+            System.out.println("Losowanie nr: "+j+" wylosowane liczby to:");
             for (i = 0; i < 6; i++) {
                 result = rnd.nextInt(49)+1;
-                System.out.printf("Wylosowane liczby to: "+result + " ");
+                System.out.print(result + " ");
             }
             System.out.println();
         }
