045c2ffc0b2c067ebcbb7c3f4b01cd982a139c7c
rajkonkret <rajkonkret@hotmail.com>
2019-09-30 09:42:56 +0200

    new

--- src/Lotto.java
+++ src/Lotto.java
@@ -3,16 +3,33 @@ import java.util.Random;
 import java.util.stream.IntStream;
 
 public class Lotto {
+
     public static void main(String[] args) {
+        int[] spool = new int[6];
         int i;
         int j;
-        int result;
+        boolean itis;
+        itis = false;
+        int result=0;
+
         Random rnd = new Random();
         System.out.println("Generator Lotto by RJ");
+        for (i=0 ; i<6 ; i++){
+            spool[i]=0;
+            }
         for (j=1; j<=100; j++) {
             System.out.println("Losowanie nr: "+j+" wylosowane liczby to:");
             for (i = 0; i < 6; i++) {
-                result = rnd.nextInt(49)+1;
+                itis = true;
+                while (itis) {
+                    itis = false;
+                    result = rnd.nextInt(49)+1;
+                    for (int k=0 ; k<6 ; k++){
+                       if (spool[k] == result) { itis = true; }
+
+                    }
+                    spool[i] = result;
+                }
                 System.out.print(result + " ");
             }
             System.out.println();
